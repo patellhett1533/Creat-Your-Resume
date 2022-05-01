2f58ef560450af5917ed59d530a17459b2e46dbf
HET PATEL <103815535+patellhett1533@users.noreply.github.com>
2022-05-01 09:58:35 +0530

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,377 @@
+
+// Resume Templete side logic
+
+
+let dark = document.getElementById('dark');
+let main = document.getElementById('main');
+let download = document.getElementById('download');
+let myImg = document.getElementById('myImg');
+
+
+
+// // Resume Form side logic
+
+let PRIVATE = document.getElementById('private');
+let CONTACT = document.getElementById('contact');
+let LANGUAGE = document.getElementById('language');
+let SKILL = document.getElementById('skill');
+let EDUCATION = document.getElementById('education');
+let EXPERIENCE = document.getElementById('experience');
+let HOBBY = document.getElementById('hobby');
+let CERTIFICATE = document.getElementById('certificate');
+let POSITION = document.getElementById('position');
+
+let next1 = document.getElementById('next1');
+let next2 = document.getElementById('next2');
+let next3 = document.getElementById('next3');
+let next4 = document.getElementById('next4');
+let next5 = document.getElementById('next5');
+let next6 = document.getElementById('next6');
+let next7 = document.getElementById('next7');
+let next8 = document.getElementById('next8');
+
+let back2 = document.getElementById('back2');
+let back3 = document.getElementById('back3');
+let back4 = document.getElementById('back4');
+let back5 = document.getElementById('back5');
+let back6 = document.getElementById('back6');
+let back7 = document.getElementById('back7');
+let back8 = document.getElementById('back8');
+let back9 = document.getElementById('back9');
+
+next1.addEventListener('click', () => {
+    PRIVATE.style.display = 'none';
+    CONTACT.style.display = 'block';
+    LANGUAGE.style.display = 'none';
+    SKILL.style.display = 'none';
+    EDUCATION.style.display = 'none';
+    EXPERIENCE.style.display = 'none';
+    HOBBY.style.display = 'none';
+    CERTIFICATE.style.display = 'none';
+    POSITION.style.display = 'none';
+})
+back2.addEventListener('click', () => {
+    PRIVATE.style.display = 'block';
+    CONTACT.style.display = 'none';
+    LANGUAGE.style.display = 'none';
+    SKILL.style.display = 'none';
+    EDUCATION.style.display = 'none';
+    EXPERIENCE.style.display = 'none';
+    HOBBY.style.display = 'none';
+    CERTIFICATE.style.display = 'none';
+    POSITION.style.display = 'none';
+})
+next2.addEventListener('click', () => {
+    PRIVATE.style.display = 'none';
+    CONTACT.style.display = 'none';
+    LANGUAGE.style.display = 'block';
+    SKILL.style.display = 'none';
+    EDUCATION.style.display = 'none';
+    EXPERIENCE.style.display = 'none';
+    HOBBY.style.display = 'none';
+    CERTIFICATE.style.display = 'none';
+    POSITION.style.display = 'none';
+})
+back3.addEventListener('click', () => {
+    PRIVATE.style.display = 'none';
+    CONTACT.style.display = 'block';
+    LANGUAGE.style.display = 'none';
+    SKILL.style.display = 'none';
+    EDUCATION.style.display = 'none';
+    EXPERIENCE.style.display = 'none';
+    HOBBY.style.display = 'none';
+    CERTIFICATE.style.display = 'none';
+    POSITION.style.display = 'none';
+})
+next3.addEventListener('click', () => {
+    PRIVATE.style.display = 'none';
+    CONTACT.style.display = 'none';
+    LANGUAGE.style.display = 'none';
+    SKILL.style.display = 'block';
+    EDUCATION.style.display = 'none';
+    EXPERIENCE.style.display = 'none';
+    HOBBY.style.display = 'none';
+    CERTIFICATE.style.display = 'none';
+    POSITION.style.display = 'none';
+})
+back4.addEventListener('click', () => {
+    PRIVATE.style.display = 'none';
+    CONTACT.style.display = 'none';
+    LANGUAGE.style.display = 'block';
+    SKILL.style.display = 'none';
+    EDUCATION.style.display = 'none';
+    EXPERIENCE.style.display = 'none';
+    HOBBY.style.display = 'none';
+    CERTIFICATE.style.display = 'none';
+    POSITION.style.display = 'none';
+})
+next4.addEventListener('click', () => {
+    PRIVATE.style.display = 'none';
+    CONTACT.style.display = 'none';
+    LANGUAGE.style.display = 'none';
+    SKILL.style.display = 'none';
+    EDUCATION.style.display = 'block';
+    EXPERIENCE.style.display = 'none';
+    HOBBY.style.display = 'none';
+    CERTIFICATE.style.display = 'none';
+    POSITION.style.display = 'none';
+})
+back5.addEventListener('click', () => {
+    PRIVATE.style.display = 'none';
+    CONTACT.style.display = 'none';
+    LANGUAGE.style.display = 'none';
+    SKILL.style.display = 'block';
+    EDUCATION.style.display = 'none';
+    EXPERIENCE.style.display = 'none';
+    HOBBY.style.display = 'none';
+    CERTIFICATE.style.display = 'none';
+    POSITION.style.display = 'none';
+})
+next5.addEventListener('click', () => {
+    PRIVATE.style.display = 'none';
+    CONTACT.style.display = 'none';
+    LANGUAGE.style.display = 'none';
+    SKILL.style.display = 'none';
+    EDUCATION.style.display = 'none';
+    EXPERIENCE.style.display = 'block';
+    HOBBY.style.display = 'none';
+    CERTIFICATE.style.display = 'none';
+    POSITION.style.display = 'none';
+})
+back6.addEventListener('click', () => {
+    PRIVATE.style.display = 'none';
+    CONTACT.style.display = 'none';
+    LANGUAGE.style.display = 'none';
+    SKILL.style.display = 'none';
+    EDUCATION.style.display = 'block';
+    EXPERIENCE.style.display = 'none';
+    HOBBY.style.display = 'none';
+    CERTIFICATE.style.display = 'none';
+    POSITION.style.display = 'none';
+})
+next6.addEventListener('click', () => {
+    PRIVATE.style.display = 'none';
+    CONTACT.style.display = 'none';
+    LANGUAGE.style.display = 'none';
+    SKILL.style.display = 'none';
+    EDUCATION.style.display = 'none';
+    EXPERIENCE.style.display = 'none';
+    HOBBY.style.display = 'block';
+    CERTIFICATE.style.display = 'none';
+    POSITION.style.display = 'none';
+})
+back7.addEventListener('click', () => {
+    PRIVATE.style.display = 'none';
+    CONTACT.style.display = 'none';
+    LANGUAGE.style.display = 'none';
+    SKILL.style.display = 'none';
+    EDUCATION.style.display = 'none';
+    EXPERIENCE.style.display = 'block';
+    HOBBY.style.display = 'none';
+    CERTIFICATE.style.display = 'none';
+    POSITION.style.display = 'none';
+})
+next7.addEventListener('click', () => {
+    PRIVATE.style.display = 'none';
+    CONTACT.style.display = 'none';
+    LANGUAGE.style.display = 'none';
+    SKILL.style.display = 'none';
+    EDUCATION.style.display = 'none';
+    EXPERIENCE.style.display = 'none';
+    HOBBY.style.display = 'none';
+    CERTIFICATE.style.display = 'block';
+    POSITION.style.display = 'none';
+})
+back8.addEventListener('click', () => {
+    PRIVATE.style.display = 'none';
+    CONTACT.style.display = 'none';
+    LANGUAGE.style.display = 'none';
+    SKILL.style.display = 'none';
+    EDUCATION.style.display = 'none';
+    EXPERIENCE.style.display = 'none';
+    HOBBY.style.display = 'block';
+    CERTIFICATE.style.display = 'none';
+    POSITION.style.display = 'none';
+})
+next8.addEventListener('click', () => {
+    PRIVATE.style.display = 'none';
+    CONTACT.style.display = 'none';
+    LANGUAGE.style.display = 'none';
+    SKILL.style.display = 'none';
+    EDUCATION.style.display = 'none';
+    EXPERIENCE.style.display = 'none';
+    HOBBY.style.display = 'none';
+    CERTIFICATE.style.display = 'none';
+    POSITION.style.display = 'block';
+})
+back9.addEventListener('click', () => {
+    PRIVATE.style.display = 'none';
+    CONTACT.style.display = 'none';
+    LANGUAGE.style.display = 'none';
+    SKILL.style.display = 'none';
+    EDUCATION.style.display = 'none';
+    EXPERIENCE.style.display = 'none';
+    HOBBY.style.display = 'none';
+    CERTIFICATE.style.display = 'block';
+    POSITION.style.display = 'none';
+})
+
+let addLang = document.getElementById('addLang');
+let getLang = document.getElementById('getLang');
+
+let langTag = "";
+addLang.addEventListener('click', ()=>{
+    if(getLang){
+        langTag += `<li>${getLang.value}</li>`;
+        getLang.value = "";
+    }
+})
+
+let addSkill = document.getElementById('addSkill');
+let getSkill = document.getElementById('getSkill');
+
+let skillTag ="";
+addSkill.addEventListener('click', ()=>{
+    if(getSkill){
+        skillTag += `<li>${getSkill.value}</li>`;
+        getSkill.value = "";
+    }
+})
+
+
+let addEdu = document.getElementById('addEdu');
+let getDegree = document.getElementById('getDegree');
+let getEduFrom = document.getElementById('getEduFrom');
+let getEduTo = document.getElementById('getEduTo');
+let getAEdu = document.getElementById('getAEdu');
+
+let EduTag = "";
+addEdu.addEventListener('click', ()=>{
+    if(getDegree || getEduFrom || getEduTo || getAEdu){
+       EduTag += `<li>
+       <h5>${getDegree.value} &nbsp;/ <span>${getEduFrom.value}-${getEduTo.value}</span></h5>
+       <p>${getAEdu.value}</p>
+   </li>`;
+   getDegree.value = "";
+   getEduFrom.value = "";
+   getEduTo.value = "";
+   getAEdu.value = "";
+    }
+})
+
+let addExp = document.getElementById('addExp');
+let getJob = document.getElementById('getJob');
+let getJobFrom = document.getElementById('getJobFrom');
+let getJobTo = document.getElementById('getJobTo');
+let getAJob = document.getElementById('getAJob');
+
+let ExpTag = "";
+addExp.addEventListener('click', ()=>{
+    if(getJob || getJobFrom || getJobTo || getAJob){
+       ExpTag += `<li>
+       <h5>${getJob.value} &nbsp;/ <span>${getJobFrom.value}-${getJobTo.value}</span></h5>
+       <p>${getAJob.value}</p>
+   </li>`;
+   getJob.value = "";
+   getJobFrom.value = "";
+   getJobTo.value = "";
+   getAJob.value = "";
+    }
+})
+
+let addHobby = document.getElementById('addHobby');
+let getHobby = document.getElementById('getHobby');
+
+let hobbyTag = "";
+addHobby.addEventListener('click', ()=>{
+    if(getHobby){
+        hobbyTag += `<li>${getHobby.value}</li>`;
+        getHobby.value = "";
+        
+    }
+})
+
+let addCerti = document.getElementById('addCerti');
+let getCerti = document.getElementById('getCerti');
+let getACerti = document.getElementById('getACerti');
+
+let certiTag = "";;
+addCerti.addEventListener('click', ()=>{
+    if(getCerti || getACerti){
+        certiTag += `<li>
+        <h5>${getCerti.value}</h5>
+        <p>${getACerti.value}</p>
+    </li>`;
+    getCerti.value = "";
+    getACerti.value = "";
+    }
+})
+
+let addAct = document.getElementById('addAct');
+let getAct = document.getElementById('getAct');
+let getAAct = document.getElementById('getAAct');
+
+let actTag = "";
+addAct.addEventListener('click', ()=>{
+    if(getAct || getAAct){
+        actTag += `<li>
+        <h5>${getAct.value}</h5>
+        <p>${getAAct.value}</p>
+    </li>`;
+    getAct.value = "";
+    getAAct.value = "";
+    console.log(actTag);
+    }
+})
+
+let Generate = document.getElementById('Generate');
+
+Generate.addEventListener('click', ()=>{
+
+    let image = document.getElementById('getPhoto').files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.onloadend = function(){
+        myImg.src = reader.result;
+    }
+
+
+    let fname = document.getElementById('getFName').value;
+    let lname = document.getElementById('getLName').value;
+    document.getElementById('setName').innerHTML = fname +" "+ lname;
+
+    let role = document.getElementById('getRole').value;
+    document.getElementById('setRole').innerHTML = role;
+
+    document.getElementById('setMob').innerHTML = document.getElementById('getMob').value;
+
+    document.getElementById('setEmail').innerHTML = document.getElementById('getEmail').value;
+
+    document.getElementById('setLoc').innerHTML = document.getElementById('getLoc').value;
+
+    document.getElementById('setLang').innerHTML = langTag;
+
+    document.getElementById('setSkill').innerHTML = skillTag;
+
+    document.getElementById('setEdu').innerHTML = EduTag;
+
+    document.getElementById('setExp').innerHTML = ExpTag;
+
+    document.getElementById('setHobby').innerHTML = hobbyTag;
+
+    document.getElementById('setCerti').innerHTML = certiTag;
+
+    document.getElementById('setAct').innerHTML = actTag;
+
+    let form = document.getElementById('cv-form');
+    form.style.display = 'none';
+    main.style.display = 'grid';
+    download.style.display = 'block';
+})
+
+let downloadCv = document.getElementById('downloadCv');
+
+downloadCv.addEventListener('click', ()=>{
+    download.style.display = 'none';
+    window.print();
+})
